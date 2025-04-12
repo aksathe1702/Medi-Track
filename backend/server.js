87afcf1cc78bae0d12e8d31d383c68607538b36d
aksathe1702 <146939601+aksathe1702@users.noreply.github.com>
2025-04-12 10:45:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -58,6 +58,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
