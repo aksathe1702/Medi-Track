3a2aefc0bb845ded068be588a72f21b412f842da
aksathe1702 <146939601+aksathe1702@users.noreply.github.com>
2025-04-12 10:38:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 27017;
 
 // const __dirname = path.resolve();
 
