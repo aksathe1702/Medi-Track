097da39fbbcf6e98eb657744afe34ac64047f57a
aksathe1702 <146939601+aksathe1702@users.noreply.github.com>
2025-04-24 21:15:03 +0530

    new changes 2

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL="https://baroquehealth-backend.onrender.com/api"
-
+export const BASE_URL = process.env.NODE_ENV === "development"
+  ? "https://baroquehealth-backend.onrender.com/api"
+  : "/api";
