51dd8cb57fa86b96810699785b10b9201a4b698d
aksathe1702 <146939601+aksathe1702@users.noreply.github.com>
2025-04-27 21:32:50 +0530

    URL changed

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -40,7 +40,7 @@ const SignUp = () => {
     e.preventDefault();
     if (validateForm()) {
       try {
-        const response = await fetch(BASE_URL + '/signup', {
+        const response = await fetch('https://medi-track-bakend.vercel.app/api/' + '/signup', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
